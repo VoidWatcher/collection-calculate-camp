a04448ce22a9d8278c0bb38bc0ac69246c772827
VoidWatcher <VoidWatcher@users.noreply.github.com>
2017-08-05 10:22:51 +0800

    update is_exist_element

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,13 +1,20 @@
 'use strict';
 var is_exist_element = function(collection,element) {
+  /**
   let length = collection.length;
   for (let i = 1; i <= Math.floor(length / 2); i++) {
     collection.splice(i, 1);
   }
+  */
 
-  if (collection.indexOf(element) != -1) {
+  let newCollection = [];
+  for (let i = 0; i < collection.length; i += 2) {
+    newCollection.push(collection[i]);
+  }
+
+  if (newCollection.indexOf(element) != -1) {
     return true;
-  } else if (collection.indexOf(element) == -1) {
+  } else {
     return false;
   }
 };
